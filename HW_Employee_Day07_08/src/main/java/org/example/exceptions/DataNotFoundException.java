19e4d5bf13e291e5cdee9c2cc4a70d70ad6589e2
Raghad Mohammed Aladawi <109043585+adawi-r@users.noreply.github.com>
2024-06-10 08:52:56 +0300

    Delete HW_Employee_Day07_08 directory

--- HW_Employee_Day07_08/src/main/java/org/example/exceptions/DataNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.example.exceptions;
-
-public class DataNotFoundException extends RuntimeException {
-
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
-//
